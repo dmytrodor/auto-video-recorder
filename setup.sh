d71f4ab8188c266d1a5e1b37ccce9ae0f760de7b
Dmytro Doroshenko <dmytrodor@pro-esupplies.com>
2024-01-12 12:41:52 +0200

    refactoring: rm redundant messages

--- setup.sh
+++ setup.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-echo "-------------------------------"
-echo "Creating run script"
-echo "-------------------------------"
+echo "Creating run script ..."
 
 cp run.sh /usr/local/bin/auto_record.sh
 chmod +x /usr/local/bin/auto_record.sh
 
-echo "-------------------------------"
-echo "Creating and runing service"
-echo "-------------------------------"
+
+echo "Creating and runing service ..."
 
 cp auto_record.service /etc/systemd/system/auto_record.service
 systemctl enable auto_record.service
